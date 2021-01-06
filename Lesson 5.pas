9bbd166912f0c9114abc4750cd8ab6be222b2986
AIabdoPr <55705726+AIabdoPr@users.noreply.github.com>
2021-01-06 03:51:46 -0800

    Update Lesson 5.pas

--- Lesson 5.pas
+++ Lesson 5.pas
@@ -1,17 +1,17 @@
 Program Lesson_5;
-	{$APPTYPE CONSOLE}
-	Var a, b, c, d: Integer;
+  {$APPTYPE CONSOLE}
+  Var a, b, c, d: Integer;
 Begin
-	a := 7;
-	b := 9;
-	c := 5;
-	d := 3;
+  a := 7;
+  b := 9;
+  c := 5;
+  d := 3;
   Writeln('Bonjour'); {Result: Bonjour}
-	Writeln(a); {Result: 7}
-	Writeln(a+2); {Result(7+2=9): 9}
-	Writeln('a+b=c'); {Result: a+b=c}
-	Writeln('a+b=', a+b); {Result(7+9=16): a+b=16}
-	Writeln(d+b); {Result(d+b=12): 12}
-	Writeln(d, '+', b, '=', d+b, ' au revoir'); {Result: 3+9=12 au revoir}
+  Writeln(a); {Result: 7}
+  Writeln(a+2); {Result(7+2=9): 9}
+  Writeln('a+b=c'); {Result: a+b=c}
+  Writeln('a+b=', a+b); {Result(7+9=16): a+b=16}
+  Writeln(d+b); {Result(d+b=12): 12}
+  Writeln(d, '+', b, '=', d+b, ' au revoir'); {Result: 3+9=12 au revoir}
   Readln;
 End.
