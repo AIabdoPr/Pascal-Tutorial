09606510bf2ca2d3936f10bd828773009f7e8f00
AIabdoPr <55705726+AIabdoPr@users.noreply.github.com>
2021-01-06 03:49:46 -0800

    Update Lesson 3.pas

--- Lesson 3.pas
+++ Lesson 3.pas
@@ -1,9 +1,9 @@
 Program Lesson_3;
-	{$APPTYPE CONSOLE}
-	Var user_text: String;
+  {$APPTYPE CONSOLE}
+  Var user_text: String;
 Begin
   write('Enter the text: ');
-	Readln(user_text);
-	WriteLn(user_text);
+  Readln(user_text);
+  WriteLn(user_text);
   Readln;
 End.
