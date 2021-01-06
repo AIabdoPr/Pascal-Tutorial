74f5c58956fe97594b792972993c54930d65e677
AIabdoPr <55705726+AIabdoPr@users.noreply.github.com>
2021-01-06 03:53:57 -0800

    Update Lesson 7.pas

--- Lesson 7.pas
+++ Lesson 7.pas
@@ -1,23 +1,23 @@
 Program Lesson_7;
-	{$APPTYPE CONSOLE}
-	Var number_A, number_B: Integer;
+  {$APPTYPE CONSOLE}
+  Var number_A, number_B: Integer;
 Begin
-	Write('<- Enter the number A = '); Readln(number_A);
-	Write('<- Enter the number B = '); Readln(number_B);
-	If(number_A > number_B) Then
-	Begin
-		Writeln('-> A > B');
-		Writeln('-> ', number_A, ' > ', number_B);
-	End;
-	If(number_A < number_B) Then
-	Begin
-	  Writeln('-> B > A');
-		Writeln('-> ', number_B, ' > ', number_A);
-	End;
-	If(number_A = number_B) Then
-	Begin
-	  Writeln('-> B = A');
-		Writeln('-> ', number_A, ' = ', number_B);
-	End;
+  Write('<- Enter the number A = '); Readln(number_A);
+  Write('<- Enter the number B = '); Readln(number_B);
+  If(number_A > number_B) Then
+  Begin
+    Writeln('-> A > B');
+    Writeln('-> ', number_A, ' > ', number_B);
+  End;
+  If(number_A < number_B) Then
+  Begin
+    Writeln('-> B > A');
+    Writeln('-> ', number_B, ' > ', number_A);
+  End;
+  If(number_A = number_B) Then
+  Begin
+    Writeln('-> B = A');
+    Writeln('-> ', number_A, ' = ', number_B);
+  End;
   Readln;
 End.
