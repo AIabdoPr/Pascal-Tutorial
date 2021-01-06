78b8a189b11656a6590756d374885e6ec394328c
AIabdoPr <55705726+AIabdoPr@users.noreply.github.com>
2021-01-06 03:51:12 -0800

    Update Lesson 4.pas

--- Lesson 4.pas
+++ Lesson 4.pas
@@ -1,14 +1,14 @@
 Program Lesson_4;
-	{$APPTYPE CONSOLE}
+  {$APPTYPE CONSOLE}
 Begin
-	{Question 1}
-	Writeln('1/ Welcome in Ghardaia');
-	{Question 2}
-	Writeln('2/ You can''t do it');
-	{Question 3}
-	Writeln('3/');
-	Writeln('  - Welcome');
-	Writeln('  - Ghardaia');
-	Writeln('  - University');
-	Readln;
+  {Question 1}
+  Writeln('1/ Welcome in Ghardaia');
+  {Question 2}
+  Writeln('2/ You can''t do it');
+  {Question 3}
+  Writeln('3/');
+  Writeln('  - Welcome');
+  Writeln('  - Ghardaia');
+  Writeln('  - University');
+  Readln;
 End.
